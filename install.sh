8dc7414320ccf87b0887239155653c2e7c96bf67
Worapol Worakunsap <worapol.wor11@gmail.com>
2025-12-17 01:35:50 +0700

    ci: add batch + force replace key

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ elif [ "$(uname)" == "Linux" ]; then
         # install eza
         sudo apt install -y gpg
         sudo mkdir -p /etc/apt/keyrings
-        wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+        wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --yes --batch --dearmor -o /etc/apt/keyrings/gierens.gpg
         echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
         sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
         sudo apt update
